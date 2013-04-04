2af55ca9d3c5f9f67f439a837bbbade5b7ce8ac9
Nicolo Orlando <nicolo.orlando+github@gmail.com>
2013-04-04 14:28:24 -0400

    New drawings

--- gen/com/nicolo/tictactoe/R.java
+++ gen/com/nicolo/tictactoe/R.java
@@ -22,11 +22,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int blank=0x7f020000;
-        public static final int blankred=0x7f020001;
-        public static final int board=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int o=0x7f020004;
-        public static final int x=0x7f020005;
+        public static final int ic_launcher=0x7f020001;
+        public static final int o=0x7f020002;
+        public static final int x=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f09001a;
