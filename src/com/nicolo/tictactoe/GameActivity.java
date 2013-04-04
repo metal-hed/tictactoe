067e67fc0d27dfeb51b23789cba56dddd1339eb3
Nicolo Orlando <nicolo.orlando+github@gmail.com>
2013-04-04 17:48:10 -0400

    Removed settings button from GameActivity

--- src/com/nicolo/tictactoe/GameActivity.java
+++ src/com/nicolo/tictactoe/GameActivity.java
@@ -154,13 +154,6 @@ public class GameActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.game, menu);
-		return true;
-	}
-
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
