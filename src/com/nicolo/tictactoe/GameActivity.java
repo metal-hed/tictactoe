4730c57fcffb84cf6623c45768532afbf988c502
Nicolo Orlando <nicolo.orlando+github@gmail.com>
2013-04-09 10:29:34 -0400

    Removed new game button listener

--- src/com/nicolo/tictactoe/GameActivity.java
+++ src/com/nicolo/tictactoe/GameActivity.java
@@ -19,32 +19,7 @@ public class GameActivity extends Activity {
 		// Add the fragment to the screen.
 		getFragmentManager().beginTransaction().add(R.id.board_fragment, new BoardFragment()).commit();
 	}
-	
-	public void newGame(View v){
-		    // Create and commit a new fragment transaction that adds the fragment for the back of
-		    // the card, uses custom animations, and is part of the fragment manager's back stack.
-
-		    getFragmentManager()
-		            .beginTransaction()
-
-		            // Replace the default fragment animations with animator resources representing
-		            // rotations when switching to the back of the card, as well as animator
-		            // resources representing rotations when flipping back to the front (e.g. when
-		            // the system Back button is pressed).
-		            .setCustomAnimations(
-		                    R.animator.card_flip_right_in, R.animator.card_flip_right_out,
-		                    R.animator.card_flip_left_in, R.animator.card_flip_left_out)
-
-		            // Replace any fragments currently in the container view with a fragment
-		            // representing the next page (indicated by the just-incremented currentPage
-		            // variable).
-		            .replace(R.id.board_fragment, new BoardFragment())
-
-		            // Commit the transaction.
-		            .commit();
 		
-	}
-	
 	/**
 	 * Set up the {@link android.app.ActionBar}.
 	 */
